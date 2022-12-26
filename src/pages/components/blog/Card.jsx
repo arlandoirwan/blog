c4310718c7d24e7d2ebfa1c6ceaf7a1009bddca2
Arlando Irwan JayaDiwalsa <jaya.arlando@gmail.com>
2022-12-26 19:16:01 +0700

    blog

---
+++ src/pages/components/blog/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './blog.css'
+import { blog } from '../../assets/data/data'
+import {
+  AiOutlineTags,
+  AiOutlineClockCircle,
+  AiOutlineComment,
+  AiOutlineShareAlt,
+} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export const Card = () => {
+  return (
+    <>
+      <section className="blog">
+        <Container>
+          <Row>
+            {blog.map((item) => (
+              <Col md={3}>
+                <div className="box boxItems" key={item.id}>
+                  <div className="img">
+                    <img src={item.cover} alt="" />
+                  </div>
+                  <div className="details">
+                    <div className="tag">
+                      <AiOutlineTags className="icon" />
+                      <a href="/">#{item.category}</a>
+                    </div>
+                    <Link to={`/details/${item.id}`} className="link">
+                      <h3>{item.title}</h3>
+                    </Link>
+                    <p>{item.desc.slice(0, 180)}...</p>
+                    <div className="date">
+                      <AiOutlineClockCircle className="icon" />{' '}
+                      <label htmlFor="">{item.date}</label>
+                      <AiOutlineComment className="icon" />{' '}
+                      <label htmlFor="">27</label>
+                      <AiOutlineShareAlt className="icon" />{' '}
+                      <label htmlFor="">SHARE</label>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
